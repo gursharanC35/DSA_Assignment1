92bb3070f84c31790155b9c6866135f615e50f29
gursharanC35 <gsingh13_be24@thapar.edu>
2025-09-06 16:19:47 +0530

    Update Ques5.cpp

--- Ques5.cpp
+++ Ques5.cpp
@@ -1,43 +1,3 @@
-// #include <iostream>
-// using namespace std;
-// int main() {
-//   int matrix1[2][3] = { {1, 4, 2}, {3, 6, 8} };
-//   int matrix2[3][2] = { {2,4}, {4,6},{3, 5}};
-//   int matrix3[2][2] = {0};
-//  for(int i = 0; i<2;i++){
-//      for(int j = 0;j<2;j++){
-//          for(int k = 0; k<3;k++){
-//               matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
-//          }
-//      }
-//  } for(int i =0;i<2;i++){
-//      for(int j = 0;j<2;j++){
-//          cout<<matrix3[i][j]<<" ";
-//      } cout<<endl;
-     
-//  } 
-//   return 0;
-// }#include <iostream>
-// using namespace std;
-// int main() {
-//   int matrix1[2][3] = { {1, 4, 2}, {3, 6, 8} };
-//   int matrix2[3][2] = { {2,4}, {4,6},{3, 5}};
-//   int matrix3[2][2] = {0};
-//  for(int i = 0; i<2;i++){
-//      for(int j = 0;j<2;j++){
-//          for(int k = 0; k<3;k++){
-//               matrix3[i][j] += matrix1[i][k] * matrix2[k][j];
-//          }
-//      }
-//  } for(int i =0;i<2;i++){
-//      for(int j = 0;j<2;j++){
-//          cout<<matrix3[i][j]<<" ";
-//      } cout<<endl;
-     
-//  } 
-//   return 0;
-// }
-
 
 #include <iostream>
 using namespace std;
@@ -63,3 +23,4 @@ int main() {
 
     return 0;
 }
+
